24431770515abea8807c193dd49d3122bbdeacea
Akash Dixit <akashdixit@localhost.localdomain>
2021-04-06 23:26:28 +0530

    get github added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const { body, validationResult } = require('express-validator');
 
@@ -311,6 +313,34 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     } catch (err) {
         console.error(err.message);
         res.status(400).json('Server Error');
+    }
+})
+
+//@route GET api/profile/github/:username
+//@desc  Get users repos from github
+//@access Private
+router.get('/github/:username', (req,res) => {
+    try {
+        
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: {'user-agent': 'node.js'}}
+
+            request(options, (error, response, body) => {
+                if(error) console.error(error);
+
+                if(response.statusCode !== 200) {
+                    res.status(404).json({msg: 'No Github Profile found'})
+                }
+
+                res.json(JSON.parse(body));
+            })
+        
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).json("Server Error")
     }
 })
 
