52887f25222859f1400d937526736d5470044d6f
Akash Dixit <akashdixit@localhost.localdomain>
2021-04-06 11:39:30 +0530

    Database connect

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+//Connect database
+connectDB();
 
  app.get('/', (req,res) => {
     res.send("API running")
@@ -9,3 +13,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+
+//
