ea5c9093de2b89b2a1719a9f54cad1cb12868203
Akash Dixit <akashdixit@localhost.localdomain>
2021-04-08 19:46:37 +0530

    Private Route Component Created

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Dashboard = props => {
+    return (
+        <div>
+            dashbaord
+        </div>
+    )
+}
+
+Dashboard.propTypes = {
+
+}
+
+export default Dashboard
